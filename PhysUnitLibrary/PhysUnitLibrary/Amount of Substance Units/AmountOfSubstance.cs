ebe30e22d268d6bbaefebc7c2cdac9b14b9fbb41
Oscar Bennich-Björkman <Oscar@OSCARS_DATOR>
2018-02-19 18:16:35 +0100

    Fixed an issue

--- PhysUnitLibrary/PhysUnitLibrary/Amount of Substance Units/AmountOfSubstance.cs
+++ PhysUnitLibrary/PhysUnitLibrary/Amount of Substance Units/AmountOfSubstance.cs
@@ -7,7 +7,7 @@ namespace PhysUnitLibrary.Amount_of_Substance_Units
     /// <summary>
     /// One of 7 physical unit dimensions
     /// </summary>
-    public abstract class LuminousIntensity
+    public abstract class AmountOfSubstance
     {
     }
 }
