7fcfefee43f2aec7d0b4e4bca35af72ffe7afade
Oscar Bennich-Björkman <Oscar@OSCARS_DATOR>
2018-02-19 18:11:04 +0100

    Added 2 units of substance

---
+++ PhysUnitLibrary/PhysUnitLibrary/Amount of Substance Units/LuminousIntensity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysUnitLibrary.Amount_of_Substance_Units
+{
+    /// <summary>
+    /// One of 7 physical unit dimensions
+    /// </summary>
+    public abstract class LuminousIntensity
+    {
+    }
+}
