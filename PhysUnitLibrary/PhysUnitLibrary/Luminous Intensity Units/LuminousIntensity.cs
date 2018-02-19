5395066bc5e069a64fcc221faa676836b59deef5
Oscar Bennich-Björkman <Oscar@OSCARS_DATOR>
2018-02-19 18:21:37 +0100

    Added unit of Candela

---
+++ PhysUnitLibrary/PhysUnitLibrary/Luminous Intensity Units/LuminousIntensity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysUnitLibrary.Luminous_Intensity_Units
+{
+    /// <summary>
+    /// One of 7 physical unit dimensions
+    /// </summary>
+    public abstract class LuminousIntensity
+    {
+    }
+}
