e9ab727c714ebff1639149eb8d6b6349d2110b39
Oscar Bennich-Björkman <Oscar@OSCARS_DATOR>
2018-02-19 18:05:27 +0100

    Added 3 units of temperature

---
+++ PhysUnitLibrary/PhysUnitLibrary/Thermodynamic Temperature Units/ThermodynamicTemperature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysUnitLibrary.Thermodynamic_Temperature_Units
+{
+    /// <summary>
+    /// One of 7 physical unit dimensions
+    /// </summary>
+    public abstract class ThermodynamicTemperature
+    {
+    }
+}
