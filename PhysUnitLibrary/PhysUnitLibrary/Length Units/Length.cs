fa0542ac7423834969921facc770404ba7297abe
Oscar Bennich-Björkman <Oscar@OSCARS_DATOR>
2018-02-19 17:37:54 +0100

    Added 25 length units (no implementation yet)

---
+++ PhysUnitLibrary/PhysUnitLibrary/Length Units/Length.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysUnitLibrary.Length_Units
+{
+    /// <summary>
+    /// One of 7 physical unit dimensions
+    /// </summary>
+    public abstract class Length
+    {
+
+    }
+}
---
+++ PhysUnitLibrary/PhysUnitLibrary/Length Units/Ångström.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysUnitLibrary.Length_Units
+{
+    class Ångström
+    {
+    }
+}
