243c16d508d6051f8b54646fd7037322f2530e17
Oscar Bennich-Björkman <Oscar@OSCARS_DATOR>
2018-02-19 18:00:38 +0100

    Added Ampere (only this for now)

---
+++ PhysUnitLibrary/PhysUnitLibrary/Electric Current Units/Ampere.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysUnitLibrary.Electric_Current_Units
+{
+    class Ampere
+    {
+
+    }
+}
---
+++ PhysUnitLibrary/PhysUnitLibrary/Electric Current Units/ElectricCurrent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysUnitLibrary.Electric_Current_Units
+{
+    /// <summary>
+    /// One of 7 physical unit dimensions
+    /// </summary>
+    public abstract class ElectricCurrent
+    {
+
+    }
+}
